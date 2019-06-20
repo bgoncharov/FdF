2c1e7b0a96452b7bc4d14a638d1efb865f2815ed
Boris Goncharov <bora4bora@gmail.com>
2019-06-19 20:25:50 -0700

    clean drawind.c

--- drawing.c
+++ drawing.c
@@ -6,7 +6,7 @@
 /*   By: bogoncha <bogoncha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 16:55:04 by bogoncha          #+#    #+#             */
-/*   Updated: 2019/06/19 20:20:35 by bogoncha         ###   ########.fr       */
+/*   Updated: 2019/06/19 20:24:46 by bogoncha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,22 @@ void	proj(t_mlx *mlx, int *xyz)
 		b = 500;
 		mlx->p.x1 = (WIN_WIDTH * 2) / 5 + (a * xyz[0] - b * xyz[1]) / 1000;
 		mlx->p.x2 = (WIN_WIDTH * 2) / 5 + (a * xyz[3] - b * xyz[4]) / 1000;
-		mlx->p.y1 = WIN_HEIGHT / 5 - xyz[2] + ((a / 2) * xyz[0] + (b / 2) * xyz[1]) / 1000;
-		mlx->p.y2 = WIN_HEIGHT / 5 - xyz[5] + ((a / 2) * xyz[3] + (b / 2) * xyz[4]) / 1000;
+		mlx->p.y1 = WIN_HEIGHT / 5 - xyz[2] + ((a / 2) * xyz[0] + (b / 2)
+		* xyz[1]) / 1000;
+		mlx->p.y2 = WIN_HEIGHT / 5 - xyz[5] + ((a / 2) * xyz[3] + (b / 2)
+		* xyz[4]) / 1000;
 	}
 	color = 65535;
 	if (xyz[2] > 0 || xyz[5] > 0)
 		color = 16761035;
-	ft_line(mlx, color);
+	line(mlx, color);
 }
 
 void		draw_map(t_mlx *mlx)
 {
-	int 		x;
-	int 		y;
-	int			xyz[6];
+	int		x;
+	int		y;
+	int		xyz[6];
 
 	if (mlx->check == 0)
 	{
@@ -66,14 +68,14 @@ void		draw_map(t_mlx *mlx)
 				xyz[3] = (x + 1) * mlx->p.gap_x;
 				xyz[4] = y * mlx->p.gap_y;
 				xyz[5] = mlx->map.tab[y][x + 1] * mlx->p.gap_z;
-				ft_proj(mlx, xyz);
+				proj(mlx, xyz);
 			}
 			if (y < (mlx->map.y_tab - 1))
 			{
 				xyz[3] = x * mlx->p.gap_x;
 				xyz[4] = (y + 1) * mlx->p.gap_y;
 				xyz[5] = mlx->map.tab[y + 1][x] * mlx->p.gap_z;
-				ft_proj(mlx, xyz);
+				proj(mlx, xyz);
 			}
 			x++;
 		}
@@ -83,7 +85,7 @@ void		draw_map(t_mlx *mlx)
 
 void	swap_xy(int *x1, int *x2, int *y1, int *y2)
 {
-	int	tmp;
+	int		tmp;
 
 	tmp = *x2;
 	*x2 = *x1;
@@ -95,27 +97,28 @@ void	swap_xy(int *x1, int *x2, int *y1, int *y2)
 
 void	line(t_mlx *mlx, int color)
 {
-	int x;
-	int y;
-	t_coord p;
+	int		x;
+	int		y;
 
-	if (p.x1 > p.x2)
-		swap_xy(&(p.x1), &(p.x2), &(p.y1), &(p.y2));
-	if ((p.x2 - p.x1) > (p.y2 - p.y1))
+	if (mlx->p.x1 > mlx->p.x2)
+		swap_xy(&(mlx->p.x1), &(mlx->p.x2), &(mlx->p.y1), &(mlx->p.y2));
+	if ((mlx->p.x2 - mlx->p.x1) > (mlx->p.y2 - mlx->p.y1))
 	{
-		x = p.x1;
-		while (x <= p.x2)
+		x = mlx->p.x1;
+		while (x <= mlx->p.x2)
 		{
-			mlx->img.data[WIN_WIDTH * (p.y1 + ((p.y2 - p.y1) * (x - p.x1 )) / (p.x2 - p.x1)) + x] = color;
+			mlx->img.data[WIN_WIDTH * (mlx->p.y1 + ((mlx->p.y2 - mlx->p.y1) *
+			(x - mlx->p.x1)) / (mlx->p.x2 - mlx->p.x1)) + x] = color;
 			x++;
 		}
 	}
-	else if ((p.y2 - p.y1) > (p.x2- p.x1))
+	else if ((mlx->p.y2 - mlx->p.y1) > (mlx->p.x2 - mlx->p.x1))
 	{
-		y = p.y1;
-		while (y <= p.y2)
+		y = mlx->p.y1;
+		while (y <= mlx->p.y2)
 		{
-			mlx->img.data[WIN_WIDTH * y + (p.x1 + ((p.x2 - p.x1) * (y - p.y1)) / (p.y2 - p.y1))] = color;
+			mlx->img.data[WIN_WIDTH * y + (mlx->p.x1 + ((mlx->p.x2 - mlx->p.x1)
+			* (y - mlx->p.y1)) / (mlx->p.y2 - mlx->p.y1))] = color;
 			y++;
 		}
 	}
